6eeab302556cc2740407d968f988933c4527ab4c
Dimitri Cross <dimitri.cross@etu.univ-grenoble-alpes.fr>
2020-05-16 23:22:24 +0200

    Updated jimmy

--- commands/jimmy.js
+++ commands/jimmy.js
@@ -19,13 +19,17 @@ module.exports = {
 		while(ri < nb){
 			ind = rsplit.findIndex( (s,i) => i > ind && s.trim() === '<span class="text">');
 			if(ind === -1) break;
-			const word = rsplit[ind+1].trim().replace( /<\/?span>/g , '');
+			const word = rsplit[ind+1].trim()
+				.replace( /<\/?span>/g , '');
+			const pron = rsplit[ind-2].trim()
+				.replace( /<span[^>]*>/g , '[')
+				.replace( /<\/span>/g , ']');
 			ind = rsplit.findIndex( (s,i) => i > ind && s.trim().startsWith("<div class='meanings-wrapper'>") );
 			const def = utils.convertFromHTML(rsplit[ind].replace( /Read more/g , ''));
 			const emb = new Discord.MessageEmbed()
 				.setColor('#FF0000')
 				.setTitle(word)
-				.setDescription(def)
+				.setDescription(`${pron}\n${def}`)
 				.setAuthor(`Result ${++ri}`, '', `https://jisho.org/word/${word}`);
 			msg.channel.send(emb);
 		}
