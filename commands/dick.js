21252c2574449cf8536ce1550b4605919b5559db
Dimitri Cross <dimitri.cross@etu.univ-grenoble-alpes.fr>
2020-05-17 23:55:26 +0200

    Updated inappropriate function

--- commands/dick.js
+++ commands/dick.js
@@ -1,3 +1,5 @@
+const utils = require('../utils.js');
+
 module.exports = {
 	name: 'dick',
 	aliases : ['cock'],
@@ -6,9 +8,21 @@ module.exports = {
 		const data = [];
 		for(const a of args){
 			let lg = 0;
-			while(94 > 100 * Math.random()) lg++;
-			const balls = (1 > 100 * Math.random()) ? 'o' : '8';
-			const rod = (lg == 0) ? '' : '='.repeat(lg - 1)+'D';
+			while(utils.prob(94, 100)) lg++;
+
+			let balls = '8';
+			if(utils.prob(1, 100)) balls = 'o';
+			if(utils.prob(1, 2000)) balls = 'ꙮ';
+
+			let baserod = '=';
+			if(utils.prob(1, 60)) baserod = '-';
+			if(utils.prob(1, 900)) baserod = '≈';
+
+			let tip = 'D';
+			if(utils.prob(1, 25)) tip = 'Đ';
+			if(utils.prob(1, 400)) tip = 'B';
+
+			const rod = (lg == 0) ? '' : baserod.repeat(lg - 1) + tip;
 			data.push(`${a} : ${balls}${rod}`);
 		}
 		msg.channel.send(data, {split: true});
--- utils.js
+++ utils.js
@@ -1,15 +1,15 @@
 
+exports.trim = (str, max) => ((str.length > max) ? `${str.slice(0, max-3)}...` : str);
 
-module.exports = {
-	trim: (str, max) => ((str.length > max) ? `${str.slice(0, max-3)}...` : str),
-
-	convertFromHTML: str => (str
+exports.convertFromHTML = str => (str
 		.replace( /<\/div>/g , '\n')
 		.replace( /&#8203/g , '\u200b')
 		.replace( /&#34;|&quot;/g , '"')
 		.replace( /&#39;|&apos;/g , "'")
 		.replace( /&#60;|&lt;/g , '<')
 		.replace( /&#62;|&gt;/g , '>')
-		.replace( /<[^>]*>/g , ''))
+		.replace( /<[^>]*>/g , ''));
 
-}
+exports.randInt = i => Math.floor(Math.random() * Math.floor(i));
+
+exports.prob = (num, den) => (num > exports.randInt(den));
